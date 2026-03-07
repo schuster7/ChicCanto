110f99e470a625cc374f4ebcdea3b75bb7b98737
Kenn Schuster <kennschuster@gmail.com>
2026-03-07 13:11:00 +0100

    Remove panel--glass from card legend panel

--- public/assets/js/card.js
+++ public/assets/js/card.js
@@ -1558,7 +1558,7 @@ function renderLegendPanel(opt){
   }).join('');
 
   return `
-    <div class="card-legend panel panel--glass">
+    <div class="card-legend panel">
       <h2>Match 3 to win</h2>
       <p class="rule-text">Scratch tiles until you reveal three matching icons</p>
       <div class="prize-list">
