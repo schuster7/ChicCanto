ef8ac483a3c823270af2360ab293622bef8d01f7
Kenn Schuster <kennschuster@gmail.com>
2026-03-07 12:12:00 +0100

    Adjust card theme settings

--- public/assets/js/card-themes.js
+++ public/assets/js/card-themes.js
@@ -40,7 +40,7 @@ export const CARD_THEMES = {
     thumbSrc: '/assets/img/thumb_women-novice-birthday1.jpg',
     bgDesktopSrc: '/assets/cards/women-novice-birthday1/bg-desktop.jpg',
     bgMobileSrc: '/assets/cards/women-novice-birthday1/bg-mobile.jpg',
-    legendPanelBg: 'rgba(34, 30, 26, .50)',
+    legendPanelBg: 'rgba(244, 237, 221, 0.8)',
     legendPanelBlur: '7px',
     prizeOptions: [
       { tier: 't1', label: 'Massage', icon: 'massage' },
